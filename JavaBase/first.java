d5a41c63d2dfc7b3811ad470330ddad856bf9a07
D3adShoT <agrajnaman1@gmail.com>
2023-12-28 21:55:23 +0530

    first commit

---
+++ JavaBase/first.java
@@ -0,0 +1,39 @@
+import java.util.*; //Scanner package
+public class first{
+    public static void main(String arg[]){
+        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        
+
+        /*
+         * Memory consumtion of each data type in Java : 
+         * Byte  - 1 Byte
+         * short - 2 Byte
+         * char - 2 Byte
+         * boolean - 1 Byte
+         * int - 4 Byte
+         * float - 4 Byte
+         * double - 8 Byte
+         * long - 8 Byte
+         * 
+         */
+
+
+
+        /* 
+         * 
+         * Default Java supports widenning conversion. i.e; Destination data type while 
+         * type conversion has to greater and compatible with the source data type.
+         *  
+         * byte (1) -> Short(2) -> int(4) -> float(4) -> long(8) -> double(8)
+         *
+         * Opposite is not allower via conversion(few cases can be Casted), called LOSSY Conversion.
+         */
+
+        // Type Casting
+        float f = 3.14f;
+        long c = (long)f;
+        System.out.println(c);
+        
+    }
+}
